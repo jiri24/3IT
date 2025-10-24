b0b755f11dcd557d2ce58c27fa052384f4b5211d
Jiří Valůšek <jiri.valusek@outlook.com>
2025-10-24 14:46:28 +0200

    Add JS in front-end

--- www/index.php
+++ www/index.php
@@ -11,8 +11,8 @@ Debugger::$logDirectory = __DIR__ . '/../zeta/logs/';
 Debugger::$strictMode = true;
 //Debugger::enable(Debugger::PRODUCTION, __DIR__ .'/../zeta/logs/');
 
-require_once '../DbConfig.php';
-require_once '../Latte.php';
+require_once '../app/DbConfig.php';
+require_once '../app/Latte.php';
 
 $urlPath = trim(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), '/');
 $className = $urlPath ? 'app\\controllers\\' . ucfirst($urlPath) : Tabulka::class;
